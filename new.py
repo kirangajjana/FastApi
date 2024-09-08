7fbf755670dad920297af5a0a647ec90a479c107
kirangajjana <kiran.gajjana@gmail.com>
2024-09-08 22:48:46 +0530

    version-3

--- new.py
+++ new.py
@@ -1,13 +1,16 @@
-from fastapi import FastAPI
+from fastapi import Body,FastAPI
 app=FastAPI()
 
 data1=[{"name":"kiran gajjana","college":"nit sikkim","city":"hyderabad"},
       {"name":"teja gajjana","college":"andhra university","city":"hyderabad"}]
+
 @app.get("/data/{bookdata}")
 def data(bookdata):
     for i in data1:
          if i.get("name").casefold()==bookdata.casefold():
               return i
+         
+
 @app.get("/data/")
 async def cato(city: str):
      data=[]
@@ -16,6 +19,9 @@ async def cato(city: str):
                data.append(i)
      return data    
                
-
+@app.post("/data/create_data")
+async def create_data(new_data=Body()):
+     data1.append(new_data)
+     
 
 
