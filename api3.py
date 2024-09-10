c80a41e0b16692bd5d41ca01e97d174f1aa211a1
kirangajjana <kiran.gajjana@gmail.com>
2024-09-10 12:01:22 +0530

    update

--- api3.py
+++ api3.py
@@ -1,3 +1,18 @@
 from fastapi import FastAPI
+import os
+from dotenv import load_dotenv
 
+load_dotenv()
 app=FastAPI()
+
+#static calling
+@app.get("/kiran")
+def name():
+    API_KEY=os.getenv("API_KEY")
+    return API_KEY
+
+#path parameters
+@app.get("/book/{name}")
+def pathpara(name: str):
+    return "welcome "+ name
+    
