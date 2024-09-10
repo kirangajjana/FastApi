c97b77e69a1fea0cc61e1e89df92c4f006ad2e90
kirangajjana <kiran.gajjana@gmail.com>
2024-09-10 14:19:46 +0530

    api3 update

--- api3.py
+++ api3.py
@@ -5,6 +5,8 @@ from dotenv import load_dotenv
 load_dotenv()
 app=FastAPI()
 
+data=[{"name":"kiran gajjana","age":25,"college":"nit sikkim","city":"Hyderabad"},{"name":"saket kumar","age":27,"college":"nit sikkim","city":"hyderabad"},{"name":"abhishek kumar","age":25,"college":"nit sikkim","city":"hyderabad"}]
+
 #static calling
 @app.get("/kiran")
 def name():
@@ -15,4 +17,11 @@ def name():
 @app.get("/book/{name}")
 def pathpara(name: str):
     return "welcome "+ name
-    
+ #query parameter
+@app.get("/college/")
+def college(college: str):
+    campus=[]
+    for i in data:
+        if i.get("college").casefold()==college.casefold():
+            campus.append(i)
+    return campus           
